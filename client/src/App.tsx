90be9178f698b018cbe41f62ee56a86b0983a3ea
Dustin Cannon <dustin.cannon@bigcommerce.com>
2026-01-31 17:04:51 -0800

    Use custom crdt-sync.svg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import { useDocument } from "@automerge/automerge-repo-react-hooks";
 import TodoList from "./TodoList";
 import { TodoDoc } from "./types";
 import "./App.css";
+// Use the public URL directly for the icon
+const crdtIcon = "/crdt-sync.svg";
 
 interface AppProps {
   docUrl: AutomergeUrl;
@@ -16,6 +18,7 @@ function App({ docUrl }: AppProps) {
     return (
       <div className="app">
         <div className="loading">
+          <img src={crdtIcon} alt="CRDT Sync" className="logo" />
           <h1>CRDT Todo List</h1>
           <p>Connecting to sync server...</p>
         </div>
@@ -26,7 +29,10 @@ function App({ docUrl }: AppProps) {
   return (
     <div className="app">
       <header className="header">
-        <h1>CRDT Todo List</h1>
+        <div className="title-row">
+          <img src={crdtIcon} alt="CRDT Sync" className="logo" />
+          <h1>CRDT Todo List</h1>
+        </div>
         <p className="subtitle">
           Powered by{" "}
           <a href="https://automerge.org" target="_blank" rel="noopener noreferrer">
